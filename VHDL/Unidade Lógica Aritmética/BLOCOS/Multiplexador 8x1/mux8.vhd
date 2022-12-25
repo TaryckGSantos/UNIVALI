449321235bdcfc5c9c7cb5cec35d1431cfa07568
TaryckGSantos <108833936+TaryckGSantos@users.noreply.github.com>
2022-12-25 19:43:49 -0300

    Update mux8.vhd

--- VHDL/Unidade Lógica Aritmética/BLOCOS/Multiplexador 8x1/mux8.vhd
+++ VHDL/Unidade Lógica Aritmética/BLOCOS/Multiplexador 8x1/mux8.vhd
@@ -17,12 +17,12 @@ end mux8;
 architecture arch_1 of mux8 is
 begin
 with i_XYZ select
-o_Q <= i_R0 when "000",
-i_R1 when "001",
-i_R2 when "010",
-i_R3 when "011",
-i_R4 when "100",
-i_R5 when "101",
-i_R6 when "110",
-i_R7 when others;
+	o_Q <= i_R0 when "000",
+		i_R1 when "001",
+		i_R2 when "010",
+		i_R3 when "011",
+		i_R4 when "100",
+		i_R5 when "101",
+		i_R6 when "110",
+		i_R7 when others;
 end arch_1;
