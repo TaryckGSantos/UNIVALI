b9c846da46867978ac2599325285752d95dc4655
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-03-13 13:10:26 -0300

    Update lista_enc.h

--- C++/Lista Encadeada/lista_enc.h
+++ C++/Lista Encadeada/lista_enc.h
@@ -5,17 +5,27 @@ template <typename TIPO>
 struct Elemento{
     TIPO dado;
     Elemento<TIPO> * proximo;
-    int tamanho;
 };
 
 template <typename TIPO>
 struct TLista{
     Elemento<TIPO> * inicio;
+    int tamanho;
 };
 
 template <typename TIPO>
 int inicializa_lista(TLista <TIPO> lista){
     lista.inicio = NULL;
+    lista.tamanho = 0;
+}
+
+template <typename TIPO>
+Elemento<TIPO> * novo_elemento_pista_de(TIPO dado){
+
+    Elemento<TIPO> * novo = new Elemento<TIPO>;
+    novo->dado = dado;
+    novo->proximo = NULL;
+
 }
 
 template <typename TIPO>
@@ -37,16 +47,7 @@ int preenche_aux(Elemento<TIPO> &aux){
 }
 
 template <typename TIPO>
-Elemento<TIPO> * novo_elemento_pista_de(TIPO dado){
-
-    Elemento<TIPO> * novo = new Elemento<TIPO>;
-    novo->dado = dado;
-    novo->proximo = NULL;
-
-}
-
-template <typename TIPO>
-int insere_final(Elemento <TIPO> &lista, TIPO dado){
+int insere_final(TLista <TIPO> &lista, TIPO dado){
 
     Elemento <TIPO> * nav = lista.inicio;
 
@@ -55,14 +56,57 @@ int insere_final(Elemento <TIPO> &lista, TIPO dado){
             nav = nav->proximo;
         }
         Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
-        preenche_novo(novo);
-        novo->proximo = NULL;
+        novo->proximo = nav->proximo;
         nav->proximo = novo;
     } else {
         Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
-        preenche_novo(novo);
-        novo->proximo = NULL;
+        novo->proximo = lista.inicio;
         lista.inicio = novo;
+    }
+}
+
+template <typename TIPO>
+int insere_inicio(TLista <TIPO> &lista, TIPO dado){
+    
+    if(lista.inicio == NULL){
+        Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
+        lista.inicio = novo;
+    } else {
+        Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
+        novo->proximo = lista.inicio;
+        lista.inicio = novo;
+    }
+}
+
+template <typename TIPO>
+int insere_inicio(TLista <TIPO> &lista, TIPO dado){
+
+    Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
+    novo->proximo = lista.inicio;
+    lista.inicio = novo;
+    lista.tamanho = lista.tamanho + 1;
+}
+
+template <typename TIPO>
+bool remove_fim(TLista <TIPO> &lista){
+    if(lista.inicio == NULL){
+        return false;
+    } else {
+        Elemento <TIPO> * nav = lista.inicio;
+        if(nav->proximo == NULL){
+            lista.inicio = nav->proximo;
+            delete nav;
+            return true;
+        } else {
+            Elemento <TIPO> * p = lista.inicio->proximo;
+            while(p->proximo != NULL){
+                nav = nav->proximo;
+                p = p->proximo;
+            }
+            nav->proximo = NULL;
+            delete p;
+            return true;
+        }
     }
 }
 
