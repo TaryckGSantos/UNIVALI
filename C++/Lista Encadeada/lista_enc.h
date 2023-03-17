defb0f620578911fa3c59a8b29fc7ffb4c640412
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-03-17 01:10:21 -0300

    Update lista_enc.h

--- C++/Lista Encadeada/lista_enc.h
+++ C++/Lista Encadeada/lista_enc.h
@@ -14,36 +14,18 @@ struct TLista{
 };
 
 template <typename TIPO>
-int inicializa_lista(TLista <TIPO> lista){
+int inicializa_lista(TLista <TIPO> &lista){
     lista.inicio = NULL;
     lista.tamanho = 0;
 }
 
 template <typename TIPO>
-Elemento<TIPO> * novo_elemento_pista_de(TIPO dado){
+Elemento<TIPO> * novo_elemento_pista_de(TIPO &dado){
 
     Elemento<TIPO> * novo = new Elemento<TIPO>;
     novo->dado = dado;
     novo->proximo = NULL;
 
-}
-
-template <typename TIPO>
-int preenche_aux(Elemento<TIPO> &aux){
-
-    cout << endl << endl << endl << "\t\t\tInsira o número da carta: ";
-    cin >> aux->dado->numero;
-    if (aux->dado->numero > 13 || aux->dado->numero < 1){
-        cout << endl << endl << endl << "\t\t\tInsira apenas cartas de 1 a 13!";
-        return novo_elemento_pista_de(aux);
-    }
-
-    cout << endl << endl <<"\t\t\t1 - Copas\n\t\t\t2 - Ouro\n\t\t\t3 - Paus\n\t\t\t4 - Espadas" << "\n\n\t\t\tInsira o naipe da carta: ";
-    cin >> aux->dado->naipe;
-    if (aux->dado->naipe > 4 || aux.dado->naipe < 1){
-        cout << endl << endl << endl << "\t\t\tInsira apenas números de 1 a 4!";
-        return novo_elemento_pista_de(aux);
-    }
 }
 
 template <typename TIPO>
@@ -56,25 +38,36 @@ int insere_final(TLista <TIPO> &lista, TIPO dado){
             nav = nav->proximo;
         }
         Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
-        novo->proximo = nav->proximo;
+        novo->proximo = NULL;
         nav->proximo = novo;
+        lista.tamanho++;
     } else {
         Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
         novo->proximo = lista.inicio;
         lista.inicio = novo;
+        lista.tamanho++;
     }
 }
 
 template <typename TIPO>
-int insere_inicio(TLista <TIPO> &lista, TIPO dado){
-    
-    if(lista.inicio == NULL){
-        Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
-        lista.inicio = novo;
+int insere_posicao(TLista <TIPO> &lista, TIPO dado, int &posicao){
+
+    Elemento <TIPO> * nav = lista.inicio;
+    Elemento <TIPO> * p = lista.inicio->proximo;
+    int aux = 0;
+
+    if(posicao-1 > lista.tamanho || posicao-1 < 0){
+        cout << "\n\n\n\t\t\tNão há como inserir neste local";
     } else {
+        while(aux > posicao-1){
+            nav = nav->proximo;
+            p = p->proximo;
+            aux++;
+        }
         Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
-        novo->proximo = lista.inicio;
-        lista.inicio = novo;
+        novo->proximo = nav->proximo;//pode ser nav->proximo
+        nav->proximo = novo;
+        lista.tamanho++;
     }
 }
 
@@ -84,19 +77,19 @@ int insere_inicio(TLista <TIPO> &lista, TIPO dado){
     Elemento<TIPO> * novo = novo_elemento_pista_de(dado);
     novo->proximo = lista.inicio;
     lista.inicio = novo;
-    lista.tamanho = lista.tamanho + 1;
+    lista.tamanho++;
 }
 
 template <typename TIPO>
-bool remove_fim(TLista <TIPO> &lista){
+int remove_fim(TLista <TIPO> &lista){
     if(lista.inicio == NULL){
-        return false;
+        cout << "Nenhuma carta inserida! ";
     } else {
         Elemento <TIPO> * nav = lista.inicio;
         if(nav->proximo == NULL){
-            lista.inicio = nav->proximo;
+            lista.inicio = NULL;
             delete nav;
-            return true;
+            lista.tamanho--;
         } else {
             Elemento <TIPO> * p = lista.inicio->proximo;
             while(p->proximo != NULL){
@@ -105,9 +98,53 @@ bool remove_fim(TLista <TIPO> &lista){
             }
             nav->proximo = NULL;
             delete p;
-            return true;
+            lista.tamanho--;
         }
     }
+}
+
+template <typename TIPO>
+int remove_inicio(TLista <TIPO> &lista){
+    if(lista.inicio == NULL){
+        cout << "Nenhuma carta inserida! ";
+    } else {
+        Elemento <TIPO> * nav = lista.inicio;
+        if(nav->proximo == NULL){
+            lista.inicio = NULL;
+            delete nav;
+            lista.tamanho--;
+        } else {
+            lista.inicio = nav->proximo;
+            delete nav;
+            lista.tamanho--;
+        }
+    }
+}
+
+template <typename TIPO>
+int remove_posicao(TLista <TIPO> &lista, TIPO dado, int &posicao_r){
+
+    Elemento <TIPO> * nav = lista.inicio;
+    Elemento <TIPO> * p = lista.inicio->proximo;
+    int aux = 0;
+
+    if(posicao_r-1 > lista.tamanho || posicao_r-1 < 0){
+        cout << "\n\n\n\t\t\tNão há como remover neste local";
+    } else {
+        while(aux-1 > posicao_r-1){
+            nav = nav->proximo;
+            p = p->proximo;
+            aux++;
+        }
+        nav->proximo = p->proximo;
+        delete p;
+        lista.tamanho--;
+    }
+}
+
+template <typename TIPO>
+int qtd_lista(TLista <TIPO> &lista){
+    return lista.tamanho;
 }
 
 #endif // LISTA_ENC_H_INCLUDED
