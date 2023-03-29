24759c211987a15839f458c928be4c493b5b6a1d
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-03-29 13:13:19 -0300

    Create inserir_produtos.cpp

---
+++ C++/Pet_shop/inserir_produtos.cpp
@@ -0,0 +1,13 @@
+    string aux_id;
+    string aux_nome;
+    string aux_descricao;
+    float aux_valor;
+
+    cout << "\n\n\n\t\t\tInsira o nome do produto: ";
+    cin >> aux_nome;
+    cout << "\n\n\t\t\tInsira o ID do produto: ";
+    cin >> aux_id;
+    cout << "\n\n\t\t\tInsira a descrição do produto: ";
+    cin >> aux_descricao;
+    cout << "\n\n\t\t\tInsira o preço do produto: ";
+    cin >> aux_valor;
