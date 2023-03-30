a23aaec5ce04bcf73cef020de50ca535c5e47bdc
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-03-29 21:33:44 -0300

    Update lista_est_generica.h

--- C++/Baralho (Lista Estática Genérica)/lista_est_generica.h
+++ C++/Baralho (Lista Estática Genérica)/lista_est_generica.h
@@ -19,34 +19,24 @@ int inicializa_lista_gen(Lista<TIPO, MAX> &el){
 
 template <typename TIPO, int MAX>
 int insere_final(Lista<TIPO, MAX> &el, Elemento <TIPO> &aux){
-
-    preenche_aux(aux);
-    verifica_insercao(el,aux);
     el.elementos[el.tamanho] = aux;
     el.tamanho++;
 }
 
 template <typename TIPO, int MAX>
 int insere_inicio(Lista<TIPO, MAX> &el, Elemento <TIPO> &aux){
-
     for(int i=el.tamanho; i>=0; i--){
         el.elementos[i] = el.elementos[i-1];
     }
-
-    preenche_aux(aux);
-    verifica_insercao(el,aux);
     el.elementos[0] = aux;
     el.tamanho++;
 }
 
 template <typename TIPO, int MAX>
 int insere_posicao(Lista<TIPO, MAX> &el, Elemento <TIPO> &aux, int aux_posicao){
-
     for(int i=el.tamanho; i>=aux_posicao; i--){
         el.elementos[i] = el.elementos[i-1];
     }
-    preenche_aux(aux);
-    verifica_insercao(el,aux);
     el.elementos[aux_posicao] = aux;
     el.tamanho++;
 }
@@ -104,52 +94,46 @@ template <typename TIPO, int MAX>
 int ordenar_bubble(Lista <TIPO, MAX> &el){
     TIPO tmp;
     int cond = 1;
-    for(int i = el.tamanho; i>= 1 and cond == 1; i--){
+    for(int i = 0; i < el.tamanho and cond == 1; i++){
         cond = 0;
-        for(int j = 0; j < i; j++){
+        for(int j = 0; j < el.tamanho-i-1; j++){
             if(el.elementos[j+1].dado < el.elementos[j].dado){
-                tmp = el.elementos[j+1].dado;
-                el.elementos[j+1].dado = el.elementos[j].dado;
-                el.elementos[j].dado = tmp;
+                tmp = el.elementos[j].dado;
+                el.elementos[j].dado = el.elementos[j+1].dado;
+                el.elementos[j+1].dado = tmp;
                 cond = 1;
             }
         }
     }
 }
 
-/*template <typename TIPO, int MAX>
-int comparador(int a, int b){
-    return
-}
-
 template <typename TIPO, int MAX>
-int particao(Lista <TIPO, MAX> &el, int inicio, int fim){
+int particao(Lista <TIPO, MAX> &el, int baixo, int alto){
+    Elemento<TIPO> pivo = el.elementos[alto];
+    int i = (baixo-1);
+    Elemento<TIPO> aux_troca;
 
-    TIPO aux_1 = el.elementos[inicio].dado;
-    int cont = 0;
-
-    for(int i = inicio + 1; i <= fim; i++){
-        if(el.elementos[inicio].dado <= aux_1){
-            cont++;
+    for (int j = baixo; j <= alto - 1; j++) {
+        if (el.elementos[j].dado < pivo.dado) {
+            i++;
+            aux_troca = el.elementos[i];
+            el.elementos[i] = el.elementos[j];
+            el.elementos[j] = aux_troca;
         }
     }
-
-    int aux_indice = inicio + cont;
-
-    TIPO tmp;
-    tmp = el.elementos[aux_indice].dado;
-    el.elementos[aux_indice].dado = el.elementos[inicio].dado;
-    el.elementos[inicio].dado = tmp;
-
-    int i = inicio; j = fim;
-
-    while( i < )
+    aux_troca = el.elementos[i+1];
+    el.elementos[i+1] = el.elementos[alto];
+    el.elementos[alto] = aux_troca;
+    return i+1;
 }
 
 template <typename TIPO, int MAX>
-int ordenar_bubble(Lista <TIPO, MAX> &el){
-    TIPO tmp;
-
-}*/
+int ordenar_quick(Lista <TIPO, MAX> &el, int baixo, int alto){
+    if(baixo < alto){
+        int aux = particao(el, baixo, alto);
+        ordenar_quick(el, baixo, aux-1);
+        ordenar_quick(el, aux+1, alto);
+    }
+}
 
 #endif // LISTA_EST_GENERICA_H_INCLUDED
