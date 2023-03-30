531b84b994cabb99dc80e9918d48373d55215a3e
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-03-29 21:55:26 -0300

    Update poker.h

--- C++/Lista Encadeada/poker.h
+++ C++/Lista Encadeada/poker.h
@@ -15,7 +15,7 @@ int mostrar_cartas(TLista <TIPO> &lista){
 
     while(nav->proximo != NULL) {
         cout << endl << endl << endl;
-        cout << "\t\t\t" << aux.dado.numero << " - ";
+        cout << "\t\t\t" << aux.dado.numero << " | ";
         switch(aux.dado.naipe){
             case 1:
                 cout<< "copas";
@@ -34,7 +34,7 @@ int mostrar_cartas(TLista <TIPO> &lista){
         aux = *nav;
     }
     cout << endl << endl << endl;
-    cout << "\t\t\t" << aux.dado.numero << " - ";
+    cout << "\t\t\t" << aux.dado.numero << " | ";
     switch(aux.dado.naipe){
         case 1:
             cout<< "copas";
@@ -114,18 +114,106 @@ int mostrar_carta_busca(TLista <TIPO> &lista, int &posicao_b){
     }
 }
 
-/*template <typename TIPO>
-int bubblesort(TLista <TIPO> &lista){
-    TIPO tmp;
-    for(int i = el.tamanho; i>= 1 and cond == 1; i--){
-        for(int j = 0; j < i; j++){
-            if(el.elementos[j+1].dado < el.elementos[j].dado){
-                tmp = el.elementos[j+1].dado;
-                el.elementos[j+1].dado = el.elementos[j].dado;
-                el.elementos[j].dado = tmp;
+template <typename TIPO>
+int menu_remover(TLista <TIPO> &lista, Elemento<TIPO> &el){
+    int aux_menu_remover;
+
+    cout << endl << endl << endl;
+    cout << "\t\t\tEscolha a maneira de remoção: ";
+    cout << endl << endl << "\t\t\t1 - Remover no início";
+    cout << endl << "\t\t\t2 - Remover no final";
+    cout << endl <<  "\t\t\t3 - Remover em uma posição específica" << endl << endl << "\t\t\t";
+    cin >> aux_menu_remover;
+
+    switch (aux_menu_remover){
+
+        case 1:
+            if(lista.tamanho == 0){
+                system("cls");
+                cout << "\n\n\n\t\t\tNenhuma carta inserida! ";
+                system("pause>nul");
+            } else {
+                remove_inicio(lista);
+                system("cls");
+                cout << "\n\n\n\t\t\tPrimeiro elemento removido! :) ";
+                system("pause>nul");
             }
-        }
+            break;
+
+        case 2:
+            if(lista.tamanho == 0){
+                system("cls");
+                cout << "\n\n\n\t\t\tNenhuma carta inserida! ";
+                system("pause>nul");
+            } else {
+                remove_fim(lista);
+                system("cls");
+                cout << "\n\n\n\t\t\tÚltimo elemento removido! :) ";
+                system("pause>nul");
+            }
+            break;
+
+        case 3:
+            if(lista.tamanho == 0){
+                system("cls");
+                cout << "\n\n\n\t\t\tNenhuma carta inserida! ";
+                system("pause>nul");
+            } else {
+                system("cls");
+                int posicao_r;
+                cout << "\n\n\n\t\t\tEscolha a posição para remover: ";
+                cin >> posicao_r;
+                if(posicao_r-1 > lista.tamanho || posicao_r-1 < 0){
+                    cout << "\n\n\n\t\t\tNão há como remover neste local";
+                    system("pause>nul");
+                } else {
+                    remove_posicao(lista, el.dado, posicao_r);
+                    system("cls");
+                    cout << "\n\n\n\t\t\tElemento na posição " << posicao_r << " removido! :) ";
+                    system("pause>nul");
+                }
+            }
+            break;
     }
-}*/
+}
+
+template <typename TIPO>
+int menu_inserir(TLista <TIPO> &lista, Elemento<TIPO> &el){
+    int aux_menu_inserir;
+
+    cout << endl << endl << endl;
+    cout << "\t\t\tEscolha a maneira de inserção: ";
+    cout << endl << endl << "\t\t\t1 - Inserir no início";
+    cout << endl << "\t\t\t2 - Inserir no final";
+    cout << endl <<  "\t\t\t3 - Inserir em uma posição específica" << endl << endl << "\t\t\t";
+    cin >> aux_menu_inserir;
+
+    switch (aux_menu_inserir){
+
+        case 1:
+            aux_inserir(el);
+            insere_inicio(lista, el.dado);
+            break;
+
+        case 2:
+            aux_inserir(el);
+            insere_final(lista, el.dado);
+            break;
+
+        case 3:
+            system("cls");
+            int posicao;
+            aux_inserir(el);
+            cout << "\n\n\n\t\t\tEscolha a posição para inserir: ";
+            cin >> posicao;
+            if(posicao-1 > lista.tamanho){
+                cout << "\n\n\n\t\t\tNão há como inserir neste local";
+                system("pause>nul");
+            } else {
+                insere_posicao(lista, el.dado, posicao);
+                break;
+            }
+    }
+}
 
 #endif // POKER_H_INCLUDED
