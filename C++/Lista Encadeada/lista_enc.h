49a9f63177057fe18b50363fe04b6b881e497c21
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-03-30 01:44:56 -0300

    Update lista_enc.h

--- C++/Lista Encadeada/lista_enc.h
+++ C++/Lista Encadeada/lista_enc.h
@@ -1,132 +1,218 @@
-#include <iostream>
-using namespace std;
-#include <stdlib.h>
-#include <time.h>
+#ifndef LISTA_ENC_H_INCLUDED
+#define LISTA_ENC_H_INCLUDED
 
-#include "lista_enc.h"
-#include "poker.h"
+template <typename TIPO>
+struct Elemento{
+    TIPO dado;
+    Elemento<TIPO> * proximo;
+};
 
-int main()
-{
-    setlocale(LC_ALL, "Portuguese");
+template <typename TIPO>
+struct TLista{
+    Elemento<TIPO> * inicio;
+    int tamanho;
+};
 
-    TLista <cartas> lista;
-    Elemento<cartas> el;
-    cartas dado, au;
+template <typename TIPO>
+int inicializa_lista(TLista <TIPO> &lista){
+    lista.inicio = NULL;
+    lista.tamanho = 0;
+}
 
-    int cont = 1, aux_menu;
+template <typename TIPO>
+Elemento<TIPO> * novo_elemento_lista_de(TIPO &dado){
 
-    inicializa_lista(lista);
+    Elemento<TIPO> * novo = new Elemento<TIPO>;
+    novo->dado = dado;
+    novo->proximo = NULL;
+}
+
+template <typename TIPO>
+int insere_final(TLista <TIPO> &lista, TIPO dado){
+
+    Elemento <TIPO> * nav = lista.inicio;
+
+    if(lista.inicio != NULL){
+        while(nav->proximo != NULL){
+            nav = nav->proximo;
+        }
+        Elemento<TIPO> * novo = novo_elemento_lista_de(dado);
+        novo->proximo = NULL;
+        nav->proximo = novo;
+        lista.tamanho++;
+    } else {
+        Elemento<TIPO> * novo = novo_elemento_lista_de(dado);
+        novo->proximo = lista.inicio;
+        lista.inicio = novo;
+        lista.tamanho++;
+    }
+}
+
+template <typename TIPO>
+int insere_inicio(TLista <TIPO> &lista, TIPO dado){
+
+    Elemento<TIPO> * novo = novo_elemento_lista_de(dado);
+    novo->proximo = lista.inicio;
+    lista.inicio = novo;
+    lista.tamanho++;
+}
+
+template <typename TIPO>
+int insere_posicao(TLista <TIPO> &lista, TIPO dado, int &posicao){
+
+    Elemento <TIPO> * nav = lista.inicio;
+    int aux = 0;
+
+    if(posicao == 1){
+        insere_inicio(lista, dado);
+    } else if (posicao == lista.tamanho){
+        insere_final(lista, dado);
+    } else {
+        while(aux < posicao-2 && nav != NULL){
+            nav = nav->proximo;
+            aux++;
+        }
+        Elemento<TIPO> * novo = novo_elemento_lista_de(dado);
+        novo->proximo = nav->proximo;
+        nav->proximo = novo;
+        lista.tamanho++;
+    }
+}
+
+template <typename TIPO>
+int remove_fim(TLista <TIPO> &lista){
+
+    Elemento <TIPO> * nav = lista.inicio;
+    if(nav->proximo == NULL){
+        lista.inicio = NULL;
+        delete nav;
+        lista.tamanho--;
+    } else {
+        Elemento <TIPO> * p = lista.inicio->proximo;
+        while(p->proximo != NULL){
+            nav = nav->proximo;
+            p = p->proximo;
+        }
+        nav->proximo = NULL;
+        delete p;
+        lista.tamanho--;
+    }
+}
+
+template <typename TIPO>
+int remove_inicio(TLista <TIPO> &lista){
+
+    Elemento <TIPO> * nav = lista.inicio;
+    if(nav->proximo == NULL){
+        lista.inicio = NULL;
+        delete nav;
+        lista.tamanho--;
+    } else {
+        lista.inicio = nav->proximo;
+        delete nav;
+        lista.tamanho--;
+    }
+}
+
+template <typename TIPO>
+int remove_posicao(TLista <TIPO> &lista, TIPO dado, int &posicao_r){
+
+    Elemento <TIPO> * nav = lista.inicio;
+    Elemento <TIPO> * p = lista.inicio->proximo;
+    int aux = 0;
+
+    if(posicao_r == 1){
+       remove_inicio(lista);
+    } else {
+        while(aux < posicao_r-2){
+            nav = nav->proximo;
+            p = p->proximo;
+            aux++;
+        }
+        if (p->proximo == NULL){
+            nav->proximo = NULL;
+        } else {
+            nav->proximo = p->proximo;
+        }
+        delete p;
+        lista.tamanho--;
+    }
+}
+
+template <typename TIPO>
+int qtd_lista(TLista <TIPO> &lista){
+    return lista.tamanho;
+}
+
+template <typename TIPO>
+int zera_baralho(TLista <TIPO> &lista){
+
+    Elemento <TIPO> * nav;
+
+    if(lista.inicio == NULL){
+        return 1;
+    } else {
+        while(nav->proximo != NULL){
+            nav = lista.inicio;
+            lista.inicio = nav->proximo;
+            delete nav;
+        }
+        delete nav;
+        lista.inicio = NULL;
+        lista.tamanho = 0;
+    }
+
+}
+
+template <typename TIPO>
+int embaralhar(TLista <TIPO> &lista){
+    int aux_emb = 0;
+
+    Elemento <TIPO> aux;
+    srand(time(NULL));
+
+    while(aux_emb<60){
+        Elemento <TIPO> * nav1 = lista.inicio;
+        Elemento <TIPO> * nav2 = lista.inicio;
+
+        int aux_pos1 = rand()%52;
+        int aux_pos2 = rand()%52;
+        int aux_nav1 = 0;
+        int aux_nav2 = 0;
+        while(aux_nav1 < aux_pos1){
+            nav1 = nav1->proximo;
+            aux_nav1++;
+        }
+        while(aux_nav2 < aux_pos2){
+            nav2 = nav2->proximo;
+            aux_nav2++;
+        }
+        aux.dado = nav1->dado;
+        nav1->dado = nav2->dado;
+        nav2->dado = aux.dado;
+        aux_emb++;
+    }
+}
+
+template <typename TIPO>
+int ordenar_bubble(TLista <TIPO> &lista){
+    TIPO tmp;
+    int cond = 1;
 
     do{
-        cout << "\n\n\t\t\t- Menu -" << endl << endl << endl;
-        cout << "\t\t\t1 - Inserir carta" << endl;
-        cout << "\t\t\t2 - Remover carta" << endl;
-        cout << "\t\t\t3 - Mostrar as cartas" << endl;
-        cout << "\t\t\t4 - Buscar alguma carta" << endl;
-        cout << "\t\t\t5 - Mostrar quantidade de cartas inseridas" << endl;
-        cout << "\t\t\t6 - Fazer um baralho completo" << endl;
-        cout << "\t\t\t7 - Embaralhar cartas" << endl;
-        /*cout << "\t\t8 - Ordenar as cartas com Bubble" << endl;*/
-        cout << "\t\t\t9 - Remover todas as cartas" << endl;
-        cout << "\t\t\t10 - Encerrar a lista" << endl << endl;
-        cout << "\t\t\tDigite o que você deseja fazer: ";
-        cin >> aux_menu;
+        cond = 0;
+        Elemento <TIPO> * nav = lista.inicio;
 
-        switch(aux_menu){
-
-            case 1:
-                system("cls");
-                menu_inserir(lista, el);
-                system("cls");
-                break;
-
-            case 2:
-                system("cls");
-                menu_remover(lista, el);
-                break;
-
-            case 3:
-                system("cls");
-                if(lista.tamanho == 0){
-                    cout << "\n\n\n\t\t\tNenhuma carta inserida! ";
-                } else {
-                    mostrar_cartas(lista);
-                }
-                system("pause>nul");
-                system("cls");
-                break;
-
-            case 4:
-                system("cls");
-                if(lista.tamanho == 0){
-                    cout << "\n\n\n\t\t\tNenhuma carta inserida! ";
-                } else {
-                    int posicao_b;
-                    Elemento <cartas> aux;
-                    cout << "\n\n\n\t\t\tEscolha a posição para buscar: ";
-                    cin >> posicao_b;
-                    mostrar_carta_busca(lista, posicao_b);
-                    system("pause>nul");
-                    system("cls");
-                }
-                break;
-
-            case 5:
-                if(lista.tamanho == 0){
-                    cout << "\n\n\n\t\t\tNenhum elemento inserido! ";
-                } else {
-                    system("cls");
-                    cout << "\n\n\n\t\t\tNúmero de elementos inseridos: " << qtd_lista(lista);
-                    system("pause>nul");
-                    system("cls");
-                }
-                break;
-
-            case 6:
-                system("cls");
-                zera_baralho(lista);
-                montar_baralho(lista);
-                cout << "\n\n\n\t\t\tBaralho montado! :)";
-                system("pause>nul");
-                system("cls");
-                break;
-
-            case 7:
-                embaralhar(lista);
-                system("cls");
-                cout << "\n\n\n\t\t\tCartas embaralhadas! :)";
-                system("pause>nul");
-                system("cls");
-                break;
-
-            /*case 8:
-                system("cls");
-                ordenar_bubble(el);
-                system("pause>nul");
-                system("cls");
-                break;*/
-
-            case 9:
-                if(lista.tamanho == 0){
-                    cout << "\n\n\n\t\t\tNenhum elemento na lista! ";
-                } else {
-                    zera_baralho(lista);
-                    system("cls");
-                    cout << "\n\n\n\t\t\tTodos os elementos removidos! :)";
-                    system("pause>nul");
-                    system("cls");
-                }
-                break;
-
-            case 10:
-                system("cls");
-                cout << "\n\n\t\tLista encerrada! :)" << "\n\n\t\t";
-                system("pause>nul");
-                exit(0);
-                break;
+        while (nav->proximo != NULL){
+            if(nav->proximo->dado < nav->dado){
+                tmp = nav->dado;
+                nav->dado = nav->proximo->dado;
+                nav->proximo->dado = tmp;
+                cond = 1;
+            }
+            nav = nav->proximo;
         }
-    } while(aux_menu != 10);
-
-    return 0;
+    } while (cond != 0);
 }
+
+#endif // LISTA_ENC_H_INCLUDED
