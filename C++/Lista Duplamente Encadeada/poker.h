70508ab987b6ba9fd73f8034d5d92ec3d0aaff69
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-03-31 02:00:12 -0300

    Update poker.h

--- C++/Lista Duplamente Encadeada/poker.h
+++ C++/Lista Duplamente Encadeada/poker.h
@@ -1,13 +1,13 @@
 #ifndef POKER_H_INCLUDED
 #define POKER_H_INCLUDED
 
-struct cartas{
+struct cartas{ /// declaração da estrutura específica
     int numero;
     int naipe;
 };
 
 template <typename TIPO>
-int mostrar_cartas(TLista <TIPO> &lista){
+int mostrar_cartas(TLista <TIPO> &lista){ /// Função para mostrar o baralho completo
 
     Elemento<TIPO> aux;
     Elemento <TIPO> * nav = lista.inicio;
@@ -52,12 +52,12 @@ int mostrar_cartas(TLista <TIPO> &lista){
 }
 
 template <typename TIPO>
-int aux_inserir(Elemento<TIPO> &el){
+int aux_inserir(Elemento<TIPO> &el){ /// Auxiliar de inserção
     system("cls");
 
     cout << endl << endl << endl << "\t\t\tInsira o número da carta: ";
     cin >> el.dado.numero;
-    if (el.dado.numero > 13 || el.dado.numero < 1){
+    if (el.dado.numero > 13 || el.dado.numero < 1){ /// Caso o numero seja maior que 13 ou menor que 1, voltar para a inserção
         cout << endl << endl << endl << "\t\t\tInsira apenas cartas de 1 a 13!";
         system("pause>nul");
         return aux_inserir(el);
@@ -65,7 +65,7 @@ int aux_inserir(Elemento<TIPO> &el){
 
     cout << endl << endl <<"\t\t\t1 - Copas\n\t\t\t2 - Ouro\n\t\t\t3 - Paus\n\t\t\t4 - Espadas" << "\n\n\t\t\tInsira o naipe da carta: ";
     cin >> el.dado.naipe;
-    if (el.dado.naipe > 4 || el.dado.naipe < 1){
+    if (el.dado.naipe > 4 || el.dado.naipe < 1){ /// Caso o naipe seja maior que 4 ou menor que 1, voltar para a inserção
         cout << endl << endl << endl << "\t\t\tInsira apenas números de 1 a 4!";
         system("pause>nul");
         return aux_inserir(el);
@@ -73,7 +73,7 @@ int aux_inserir(Elemento<TIPO> &el){
 }
 
 template <typename TIPO>
-int verifica_insercao(TLista <TIPO> &lista, Elemento<TIPO> &el){
+int verifica_insercao(TLista <TIPO> &lista, Elemento<TIPO> &el){ /// Auxiliar para a verificação de elementos repetidos
     Elemento <TIPO> * nav = lista.inicio;
 
     while(nav != NULL){
@@ -87,7 +87,7 @@ int verifica_insercao(TLista <TIPO> &lista, Elemento<TIPO> &el){
 }
 
 template <typename TIPO>
-int montar_baralho(TLista <TIPO> &lista){
+int montar_baralho(TLista <TIPO> &lista){ /// Monta um baralho completo utilizando o insere final
 
     cartas aux;
 
@@ -101,7 +101,7 @@ int montar_baralho(TLista <TIPO> &lista){
 }
 
 template <typename TIPO>
-int mostrar_carta_busca(TLista <TIPO> &lista, int &posicao_b){
+int mostrar_carta_busca(TLista <TIPO> &lista, int &posicao_b){ /// Função que mostra uma carta em determianda posição
     int aux = 0;
 
     Elemento <TIPO> aux_b;
@@ -109,23 +109,23 @@ int mostrar_carta_busca(TLista <TIPO> &lista, int &posicao_b){
     Elemento <TIPO> * nav_inicio = lista.inicio;
     Elemento <TIPO> * nav_final = lista.fim;
 
-    if(posicao_b > lista.tamanho || posicao_b-1 < 0){
+    if(posicao_b > lista.tamanho || posicao_b-1 < 0){ /// Caso a posição seja maior ou menor que as existentes
         cout << "\n\n\n\t\t\tNão há nenhuma carta neste local";
     } else {
-        if(posicao_b-1 > (lista.tamanho/2)-1){
+        if(posicao_b-1 > (lista.tamanho/2)-1){ /// Busca para posição maior que a metade
             aux = lista.tamanho-1;
             while(aux < posicao_b-1){
                 nav_final = nav_final->anterior;
             }
             aux_b = *nav_final;
-        } else {
+        } else { /// Busca para posição menor que a metade
             while(aux > posicao_b-1){
                 nav_inicio = nav_inicio->proximo;
             }
             aux_b = *nav_inicio;
         }
 
-        cout << "\n\n\n\t\t\tCarta nessa posição: " << aux_b.dado.numero << " - ";
+        cout << "\n\n\n\t\t\tCarta nessa posição: " << aux_b.dado.numero << " - "; /// Imprime a carta
         switch(aux_b.dado.naipe){
             case 1:
                 cout<< "copas";
@@ -144,7 +144,7 @@ int mostrar_carta_busca(TLista <TIPO> &lista, int &posicao_b){
 }
 
 template <typename TIPO>
-int menu_inserir(TLista <TIPO> &lista, Elemento<TIPO> &el){
+int menu_inserir(TLista <TIPO> &lista, Elemento<TIPO> &el){ /// Menu de inserção
     int aux_menu_inserir;
     int aux_conf = 0;
 
@@ -160,10 +160,10 @@ int menu_inserir(TLista <TIPO> &lista, Elemento<TIPO> &el){
         case 1:
             aux_inserir(el);
             aux_conf = verifica_insercao(lista, el);
-            if(aux_conf == 0){
+            if(aux_conf == 0){ /// caso não seja carta repetida
                 insere_inicio(lista, el.dado);
                 system("cls");
-            } else {
+            } else { /// caso seja carta repetida
                 cout << endl << endl << endl << "\t\t\tEste elemento já existe! ";
                 system("pause>nul");
                 system("cls");
@@ -190,7 +190,7 @@ int menu_inserir(TLista <TIPO> &lista, Elemento<TIPO> &el){
             if(aux_conf == 0){
                 cout << "\n\n\n\t\t\tEscolha a posição para inserir: ";
                 cin >> posicao;
-                if(posicao-1 > lista.tamanho || posicao-1 < 0){
+                if(posicao-1 > lista.tamanho || posicao-1 < 0){ /// Caso a posição esteja fora dos padrões
                     cout << "\n\n\n\t\t\tPosição impossível de inserir! :(";
                     system("pause>nul");
                     system("cls");
@@ -208,7 +208,7 @@ int menu_inserir(TLista <TIPO> &lista, Elemento<TIPO> &el){
 }
 
 template <typename TIPO>
-int menu_remover(TLista <TIPO> &lista, Elemento<TIPO> &el){
+int menu_remover(TLista <TIPO> &lista, Elemento<TIPO> &el){ /// menu de remoção
     int aux_menu_remover;
 
     cout << endl << endl << endl;
@@ -221,12 +221,12 @@ int menu_remover(TLista <TIPO> &lista, Elemento<TIPO> &el){
     switch (aux_menu_remover){
 
         case 1:
-            if(lista.tamanho == 0){
+            if(lista.tamanho == 0){ /// caso o baralho esteja zerado
                 system("cls");
                 cout << "\n\n\n\t\t\tNenhuma carta inserida! ";
                 system("pause>nul");
                 system("cls");
-            } else {
+            } else { /// caso não esteja
                 remove_inicio(lista);
                 system("cls");
                 cout << "\n\n\n\t\t\tPrimeiro elemento removido! :) ";
@@ -261,7 +261,7 @@ int menu_remover(TLista <TIPO> &lista, Elemento<TIPO> &el){
                 int posicao_r;
                 cout << "\n\n\n\t\t\tEscolha a posição para remover: ";
                 cin >> posicao_r;
-                if(posicao_r > lista.tamanho || posicao_r-1 < 0){
+                if(posicao_r > lista.tamanho || posicao_r-1 < 0){ /// Caso a posição esteja fora dos padrões
                     cout << "\n\n\n\t\t\tNão há como remover neste local";
                     system("pause>nul");
                     system("cls");
@@ -277,11 +277,11 @@ int menu_remover(TLista <TIPO> &lista, Elemento<TIPO> &el){
     }
 }
 
-bool operator < (cartas a, cartas b){
+bool operator < (cartas a, cartas b){ /// operator de comparação
     return a.naipe*200+a.numero < b.naipe*200+b.numero;
 }
 
-bool operator == (cartas a, cartas b){
+bool operator == (cartas a, cartas b){ /// operator de igualdade
     return a.naipe*200+a.numero == b.naipe*200+b.numero;
 }
 
