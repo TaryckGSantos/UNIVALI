44509c2a4179ba556f9df3a36f0a0f3e6dbfeaf5
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-05-08 13:12:03 -0300

    Update main.cpp

--- C++/Atendimento a Torcedores/main.cpp
+++ C++/Atendimento a Torcedores/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-#include <time.h>
 using namespace std;
 
-#include "Atendimento.h"
-#include "Torcedor.h"
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <iostream>
 
+#include "atendimento.h"
+#include "torcedor.h"
 
 int main()
 {
@@ -13,6 +17,7 @@ int main()
 
     Torcedor torcedores;
     int select=0,carga_inicial,tempoSim,numGST,numGN,variavel;
+    char tecla;
 
     cout<<"\n\n\t\tQuantidade de Guichês Sócio Torcedor: ";
     cin>>numGST;
@@ -40,6 +45,9 @@ int main()
     inicializa_fila(fila);
     montar_fila(fila,carga_inicial);
 
+    system("pause>nul");
+    system("cls");
+
     do{
         cout << "\n\n\t\t\t- MENU FILA -" << endl << endl << endl;
         cout << "\t\t\t1 - Inserir Pessoa no Final da Fila" << endl;
@@ -49,27 +57,36 @@ int main()
         cout << "\t\t\t5 - Rodar uma Unidade de Tempo" << endl;
         cout << "\t\t\t6 - Zerar" << endl;
         cout << "\t\t\t7 - Encerrar Programa" << endl;
-        cin>>select;
-        cout<<endl<<endl;
-        switch(select){
-        case 1:
+        select = getch(); // Verifica comando do jogador
+
+        if (select == '1') {
             entrar_na_fila(fila,torcedores,carga_inicial);
-            break;
-        case 2:
+            system("pause>nul");
+            system("cls");
+        } else if (select == '2') {
             atender(fila);
-            break;
-        case 3:
+            system("pause>nul");
+            system("cls");
+        } else if (select == '3') {
             imprime_fila(fila);
-            break;
-        case 4:
+            system("pause>nul");
+            system("cls");
+        } else if (select == '4') {
             primeiro(fila,torcedores);
-            break;
-        case 5:
+            system("pause>nul");
+            system("cls");
+        } else if (select == '5') {
             rodar_tempo(fila);
-            break;
-        case 6:
+            cout << "\n\n\tTempo rodado!";
+            system("pause>nul");
+            system("cls");
+        } else if (select == '6') {
             zerar();
+            system("pause>nul");
+            system("cls");
             break;
+        } else if (select == '7') {
+            return 0;
         }
     }while(select!=7);
     return 0;
