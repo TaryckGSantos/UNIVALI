8dd611fd94e31df810c37dbc61244b621242e3da
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-05-10 13:06:10 -0300

    Update main.cpp

--- C++/Atendimento a Torcedores/main.cpp
+++ C++/Atendimento a Torcedores/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
-
 #include <time.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <windows.h>
 #include <iostream>
+#include "Atendimento.h"
+#include "Torcedor.h"
 
-#include "atendimento.h"
-#include "torcedor.h"
 
 int main()
 {
@@ -16,8 +15,7 @@ int main()
     setlocale(LC_ALL,"Portuguese");
 
     Torcedor torcedores;
-    int select=0,carga_inicial,tempoSim,numGST,numGN,variavel;
-    char tecla;
+    int select=0,carga_inicial,tempoSim,numGST,numGN,cont=0,qtdpTEMP,ins,auxEntrarN=0,auxEntrarS=0;;
 
     cout<<"\n\n\t\tQuantidade de Guichês Sócio Torcedor: ";
     cin>>numGST;
@@ -25,69 +23,190 @@ int main()
     cin>>numGN;
     cout<<"\n\n\t\tQuantidade de Pessoas que estão aguardando antes do ínicio da simulação(carga inicial): ";
     cin>>carga_inicial;
-    ///cout<<"\n\n\t\tQuantidade de Pessoas que procuram os Guichês a cada unidade de Tempo: ";
-    ///cin>>;
-    ///cout<<"\n\n\t\tQuantidade de Tempo em que o Modelo será 'Simulado': ";
-    ///cin>>tempoSim;
-
+    cout<<"\n\n\t\tQuantidade de Pessoas que procuram os Guichês a cada unidade de Tempo: ";
+    cin>>qtdpTEMP;
+    cout<<"\n\n\t\tQuantidade de Tempo em que o Modelo será 'Simulado': ";
+    cin>>tempoSim;
+    ins=qtdpTEMP;
     TFila<Torcedor> fila;
-    variavel=numGST+numGN;
 
-    TFila<Torcedor> *vet;
-    vet = new TFila<Torcedor>[variavel];
+    ///CRIAÇÃO DAS FILAS SÓCIO TORCEDOR
+    TFila<Torcedor> *filaST;
+    filaST = new TFila<Torcedor>[numGST];
+
     for(int i=0;i<numGST;i++){
-        vet[i]=fila;    ///0 é igual a Guichê Sócio Torcedor
+        filaST[i]=fila;
     }
+
+    for(int i=0;i<numGST;i++){
+        inicializa_fila(filaST[i]);
+    }
+
+    ///CRIAÇÃO DAS FILAS NORMAIS
+    TFila<Torcedor> *filaN;
+    filaN = new TFila<Torcedor>[numGN];
+
     for(int i=0;i<numGN;i++){
-                     ///1 é igual a Guichê Normal
+        filaN[i]=fila;
     }
 
-    inicializa_fila(fila);
-    montar_fila(fila,carga_inicial);
+    for(int i=0;i<numGN;i++){
+        inicializa_fila(filaN[i]);
+    }
 
-    system("pause>nul");
+    ///MONTAR AS FILAS
+    int auxST,auxN;
+    auxST=carga_inicial/20;
+    auxN=carga_inicial-auxST;
+    cout<<"\nauxST: "<<auxST;
+    cout<<"\nauxN: "<<auxN;
+
+    while(auxST!=0){
+        if(cont==numGST){
+            cont=0;
+        }
+        montar_filaST(filaST[cont],torcedores,auxST);
+        entrar_na_fila(filaST[cont],torcedores);
+        cont++;
+    }
+    cont=0;
+    while(auxN!=0){
+        if(cont==numGN){
+            cont=0;
+        }
+        montar_filaN(filaN[cont],torcedores,auxN);
+        entrar_na_fila(filaN[cont],torcedores);
+        cont++;
+    }
+    cont=0;
     system("cls");
-
     do{
-        cout << "\n\n\t\t\t- MENU FILA -" << endl << endl << endl;
+        cout << "\n\n\t\t\t- MENU FILA -\t\t\t\tTempo: "<<tempoSim << endl << endl << endl;
         cout << "\t\t\t1 - Inserir Pessoa no Final da Fila" << endl;
-        cout << "\t\t\t2 - Remover Pessoa do Inicio da Fila" << endl;
-        cout << "\t\t\t3 - Mostrar pessoas na Fila" << endl;
-        cout << "\t\t\t4 - Primeira Posição" <<endl;
-        cout << "\t\t\t5 - Rodar uma Unidade de Tempo" << endl;
-        cout << "\t\t\t6 - Zerar" << endl;
+        cout << "\t\t\t2 - Mostrar pessoas na Fila" << endl;
+        cout << "\t\t\t3 - Primeira Posição" <<endl;
+        cout << "\t\t\t4 - Rodar uma Unidade de Tempo" << endl;
+        cout << "\t\t\t5 - Zerar" << endl;
+        cout << "\t\t\t6 - Simular 1 Tempo" << endl;
         cout << "\t\t\t7 - Encerrar Programa" << endl;
         select = getch(); // Verifica comando do jogador
 
         if (select == '1') {
-            entrar_na_fila(fila,torcedores,carga_inicial);
+            qtdpTEMP=ins;
+            while(qtdpTEMP!=0){
+                if(cont%20==0){
+                    montar_filaST(filaST[auxEntrarS],torcedores,qtdpTEMP);
+                    entrar_na_fila(filaST[auxEntrarS],torcedores);
+                    auxEntrarS++;
+                    if(auxEntrarS==numGST){
+                        auxEntrarS=0;
+                    }
+                }
+                else{
+                    montar_filaN(filaN[auxEntrarN],torcedores,qtdpTEMP);
+                    entrar_na_fila(filaN[auxEntrarN],torcedores);
+                    auxEntrarN++;
+                    if(auxEntrarN==numGN){
+                        auxEntrarN=0;
+                    }
+                }
+                cont++;
+            }
             system("pause>nul");
             system("cls");
-        } else if (select == '2') {
-            atender(fila);
+       } else if (select == '2') {
+            for(int i=0;i<numGST;i++){
+                cout<<"\nGuichê Sócio-Torcedor "<<i+1<<": \n\n";
+                imprime_fila(filaST[i]);
+            }
+            for(int i=0;i<numGN;i++){
+                cout<<"\nGuichê Normal "<<i+1<<": \n\n";
+                imprime_fila(filaN[i]);
+            }
             system("pause>nul");
             system("cls");
-        } else if (select == '3') {
-            imprime_fila(fila);
+       } else if (select == '3') {
+            for(int i=0;i<numGST;i++){
+                cout<<"\nGuichê Sócio-Torcedor "<<i+1<<": \n";
+                imprimir_pFila(filaST[i].inicio->dado);
+            }
+            for(int i=0;i<numGN;i++){
+                cout<<"\nGuichê Normal "<<i+1<<": \n";
+                imprimir_pFila(filaN[i].inicio->dado);
+            }
             system("pause>nul");
             system("cls");
-        } else if (select == '4') {
-            primeiro(fila,torcedores);
+       } else if (select == '4') {
+            for(int i=0;i<numGST;i++){
+                rodar_tempo(filaST[i]);
+            }
+            for(int i=0;i<numGN;i++){
+                rodar_tempo(filaN[i]);
+            }
             system("pause>nul");
             system("cls");
-        } else if (select == '5') {
-            rodar_tempo(fila);
-            cout << "\n\n\tTempo rodado!";
+       } else if (select == '5') {
+            for(int i=0;i<numGN;i++){
+                zerar(filaN[i]);
+            }
+            for(int i=0;i<numGST;i++){
+                zerar(filaST[i]);
+            }
             system("pause>nul");
             system("cls");
-        } else if (select == '6') {
-            zerar();
+            ///numGST=0;
+            ///numGN=0;
+       } else if (select == '6') {
+            system("cls");
+            qtdpTEMP=ins;
+            while(qtdpTEMP!=0){
+                if(cont%20==0){
+                    montar_filaST(filaST[auxEntrarS],torcedores,qtdpTEMP);
+                    entrar_na_fila(filaST[auxEntrarS],torcedores);
+                    auxEntrarS++;
+                    if(auxEntrarS==numGST){
+                        auxEntrarS=0;
+                    }
+                }
+                else{
+                    montar_filaN(filaN[auxEntrarN],torcedores,qtdpTEMP);
+                    entrar_na_fila(filaN[auxEntrarN],torcedores);
+                    auxEntrarN++;
+                    if(auxEntrarN==numGN){
+                        auxEntrarN=0;
+                    }
+                }
+                cont++;
+            }
+            for(int i=0;i<numGST;i++){
+                cout<<"\nGuichê Sócio-Torcedor "<<i+1<<": \n";
+                imprime_fila(filaST[i]);
+            }
+            for(int i=0;i<numGN;i++){
+                cout<<"\nGuichê Normal "<<i+1<<": \n";
+                imprime_fila(filaN[i]);
+            }
+            for(int i=0;i<numGST;i++){
+                rodar_tempo(filaST[i]);
+            }
+            for(int i=0;i<numGN;i++){
+                rodar_tempo(filaN[i]);
+            }
             system("pause>nul");
             system("cls");
-            break;
         } else if (select == '7') {
-            return 0;
+            system("cls");
+            cout << "\n\n\t\tPrograma encerrado!";
+            system("pause>nul");
+            exit(0);
+        } else {
+           system("cls");
+           cout << "\n\n\t\tPressione apenas os números de 1 a 7!";
+           system("pause>nul");
+           system("cls");
+           tempoSim++;
         }
-    }while(select!=7);
+        tempoSim--;
+    }while(select!=7 && tempoSim!=0);
     return 0;
 }
