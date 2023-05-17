b966a16930e0811ebfae974f391b61eaba687eaf
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-05-17 20:33:01 -0300

    Update testes.h

--- POO/front-end cinema/testes.h
+++ POO/front-end cinema/testes.h
@@ -94,10 +94,6 @@ int compra_ingressos(filmes vet_filmes[]){
     }
 }
 
-int menu_filmes(){
-
-}
-
 int menu_cliente(filmes vet_filmes[]){
     int menu_cliente = 0, horarios = 0, consulta_horario = 0, escolha;
 
@@ -144,17 +140,242 @@ int confirma_identidade(){
         if (senha == padrao_senha){
             return 5;
         } else {
+            cout << "\n\n\n\t\t\tSenha incorreta! ";
             return 4;
         }
     } else {
+        cout << "\n\n\n\t\t\tEste usuário não consta no nosso sistema! ";
         return 3;
     }
 }
 
-int menu_admin(filmes vet_filmes[]){
-    int aux = 0;
+
+
+int criar_filme(){
+    string nome, isDublado, is3d, data;
+    int id, duracao;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t| MENU ADMIN - CRIAR FILME |";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o nome do novo filme: ";
+    cin >> nome;
+    cout << "\n\n\t\t\t. Insira o ID do filme: ";
+    cin >> id;
+    cout << "\n\n\t\t\t. O filme é dublado? ";
+    cin >> isDublado;
+    cout << "\n\n\t\t\t. O filme é 3D? ";
+    cin >> is3d;
+    cout << "\n\n\t\t\t. Data: ";
+    cin >> data;
+    cout << "\n\n\t\t\t. Duração do filme (em minutos): ";
+    cin >> duracao;
+}
+
+int excluir_filme(){
+    int aux_id;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t|MENU ADMIN - EXCLUIR FILME|";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID do filme que será excluído: ";
+    cin >> aux_id;
+    /*for(int i=0, i<MAX; i++){
+        if(aux_id == vet_filmes[i].id){
+            cout << "\n\n\n\n\t\t\t '" << vet_filmes[i].nome << "' excluído!";
+            delete vet_filmes[i]; /// USAR O .ERASE
+            return 1;
+        }
+        return 0;
+    }*/
+}
+
+int buscar_filme(){
+    int aux_busca;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t| MENU ADMIN - BUSCA FILME |";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID a ser buscado: ";
+    cin >> aux_busca;
+    /*for(int i=0, i<MAX; i++){
+        if(aux_busca == vet_filmes[i].id){
+            cout << "\n\n\n\n\t\t\tFilme: '" << vet_filmes[i].nome << "' ";
+            return 1;
+        }
+        return 0;
+    }*/
+}
+
+int criar_sala(){
+
+    int id, qtd_assentos;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t|  MENU ADMIN - CRIAR SALA |";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID da nova sala: ";
+    cin >> id;
+    cout << "\n\n\t\t\t. Insira a quantidade de assentos da sala: ";
+    cin >> qtd_assentos;
+}
+
+int buscar_sala(){
+    int aux_busca;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t|  MENU ADMIN - BUSCA SALA |";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID a ser buscado: ";
+    cin >> aux_busca;
+    /*for(int i=0, i<MAX; i++){
+        if(aux_busca == vet_salas[i].id){
+            cout << "\n\n\n\n\t\t\tSala: '" << vet_salas[i].id << "' existe!";
+            return 1;
+        }
+        return 0;
+    }*/
+}
+
+int excluir_sala(){
+    int aux_id;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t| MENU ADMIN - EXCLUIR SALA|";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID da sala que será excluída: ";
+    cin >> aux_id;
+    /*for(int i=0, i<MAX; i++){
+        if(aux_id == vet_salas[i].id){
+            cout << "\n\n\n\n\t\t\tSala '" << vet_salas[i].nome << "' excluída!";
+            delete vet_salas[i]; /// USAR O .ERASE
+            return 1;
+        }
+        return 0;
+    }*/
+}
+
+int criar_sessao(){
+
+    int id, qtd_assentos_disp;
+    string filme, sala;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t| MENU ADMIN - CRIAR SESSÃO|";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID da nova sessão: ";
+    cin >> id;
+    cout << "\n\n\t\t\t. Qual é o filme que será exibido nessa sessão? ";
+    cin >> filme;
+    cout << "\n\n\t\t\t. Qual é a sala que será utilizada nessa sessão? ";
+    cin >> sala;
+    cout << "\n\n\t\t\t. Insira a quantidade de assentos disponíveis nessa sala: ";
+    cin >> qtd_assentos_disp;
+}
+
+int excluir_sessao(){
+    int aux_id;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t|MENU ADMIN  EXCLUIR SESSÃO|";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID da sessão que será excluída: ";
+    cin >> aux_id;
+    /*for(int i=0, i<MAX; i++){
+        if(aux_id == vet_sessoes[i].id){
+            cout << "\n\n\n\n\t\t\tSessão '" << vet_sessoes[i].nome << "' excluída!";
+            delete vet_sessao[i]; /// USAR O .ERASE
+            return 1;
+        }
+        return 0;
+    }*/
+}
+
+int buscar_sessao(){
+    int aux_busca;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t| MENU ADMIN - BUSCA SESSÃO|";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID a ser buscado: ";
+    cin >> aux_busca;
+    /*for(int i=0, i<MAX; i++){
+        if(aux_busca == vet_ssessoes[i].id){
+            cout << "\n\n\n\n\t\t\tA sessão: '" << vet_sessoes[i].id << "' existe!";
+            return 1;
+        }
+        return 0;
+    }*/
+}
+
+int criar_cliente(){
+
+    int id, idade;
+    string nome, pcd;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t|MENU ADMIN - CRIAR CLIENTE|";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o nome do novo cliente: ";
+    cin >> nome;
+    cout << "\n\n\t\t\t. Insira a idade do novo cliente: ";
+    cin >> idade;
+    cout << "\n\n\t\t\t. Insira o novo ID do cliente: ";
+    cin >> id;
+    cout << "\n\n\t\t\t. Esse cliente é pcd? ";
+    cin >> pcd;
+}
+
+int excluir_cliente(){
+    int aux_id;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t|MENU ADMIN.EXCLUIR CLIENTE|";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID do cliente que será excluído: ";
+    cin >> aux_id;
+    /*for(int i=0, i<MAX; i++){
+        if(aux_id == vet_clientes[i].id){
+            cout << "\n\n\n\n\t\t\tCliente '" << vet_clientes[i].nome << "' excluído!";
+            delete vet_clientes[i]; /// USAR O .ERASE
+            return 1;
+        }
+        return 0;
+    }*/
+}
+
+int buscar_cliente(){
+    int aux_busca;
+
+    system("cls");
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\t\t\t\t\t|MENU ADMIN - BUSCA CLIENTE|";
+    cout << "\n\t\t\t\t\t----------------------------";
+    cout << "\n\n\n\n\t\t\t. Insira o ID a ser buscado: ";
+    cin >> aux_busca;
+    /*for(int i=0, i<MAX; i++){
+        if(aux_busca == vet_clientes[i].id){
+            cout << "\n\n\n\n\t\t\tO cliente: '" << vet_clientes[i].id << "' existe!";
+            return 1;
+        }
+        return 0;
+    }*/
+}
+
+int menu_admin(filmes vet_filmes[], int &aux){
+    int aux_excluir, aux_buscar;
     string aux_menu;
-    aux = confirma_identidade();
 
     if (aux == 5){
         system("cls");
@@ -169,7 +390,8 @@ int menu_admin(filmes vet_filmes[]){
         cout << "\n\n\t\t\t5. Voltar no menu inicial\n\n\t\t\t";
         cin >> aux_menu;
 
-        if(aux_menu == "1"){
+        if(aux_menu == "1"){ /// Filmes
+            system("cls");
             string aux_menu_filmes;
             cout << "\n\t\t\t\t\t----------------------------";
             cout << "\n\t\t\t\t\t|    MENU ADMIN - FILMES   |";
@@ -182,40 +404,163 @@ int menu_admin(filmes vet_filmes[]){
             cin >> aux_menu_filmes;
 
             if(aux_menu_filmes == "1"){
-
+                criar_filme();
+                menu_admin(vet_filmes, aux);
             } else if(aux_menu_filmes == "2"){
-
+                aux_excluir = excluir_filme();
+                if(aux_excluir == 0){
+                    cout << "\n\n\n\n\t\t\tNenhum filme com esse ID!";
+                    system("pause>nul");
+                }
+                menu_admin(vet_filmes, aux);
             } else if(aux_menu_filmes == "3"){
-
+                aux_buscar = buscar_filme();
+                if(aux_buscar == 0){
+                    cout << "\n\n\n\n\t\t\tNenhum filme com esse ID!";
+                    system("pause>nul");
+                }
+                menu_admin(vet_filmes, aux);
             } else if(aux_menu_filmes == "4"){
-                return 7;
-            } else {
-                return menu_filmes();
+                menu_admin(vet_filmes, aux);
             }
+        } else if (aux_menu == "2"){ /// Sessões
+            system("cls");
+            string aux_menu_sessoes;
+            cout << "\n\t\t\t\t\t----------------------------";
+            cout << "\n\t\t\t\t\t|    MENU ADMIN - SESSÕES  |";
+            cout << "\n\t\t\t\t\t----------------------------";
+            cout << "\n\n\n\n\t\t\t . Escolha o que deseja fazer: \n\n";
+            cout << "\n\n\n\t\t\t1. Criar uma nova sessão";
+            cout << "\n\n\t\t\t2. Excluir uma sessão";
+            cout << "\n\n\t\t\t3. Buscar uma sessão";
+            cout << "\n\n\t\t\t4. Voltar no menu anterior\n\n\t\t\t";
+            cin >> aux_menu_sessoes;
 
-        } else if (aux_menu == "2"){
+            if(aux_menu_sessoes == "1"){
+                criar_sessao();
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_sessoes == "2"){
+                aux_excluir = excluir_sessao();
+                if(aux_excluir == 0){
+                    cout << "\n\n\n\n\t\t\tNenhuma sessão com esse ID!";
+                    system("pause>nul");
+                }
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_sessoes == "3"){
+                aux_buscar = buscar_sessao();
+                if(aux_buscar == 0){
+                    cout << "\n\n\n\n\t\t\tNenhuma sessão com esse ID!";
+                    system("pause>nul");
+                }
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_sessoes == "4"){
+                menu_admin(vet_filmes, aux);
+            }
+        } else if (aux_menu == "3"){ /// Salas
+            system("cls");
+            string aux_menu_salas;
 
-        } else if (aux_menu == "3"){
+            cout << "\n\t\t\t\t\t----------------------------";
+            cout << "\n\t\t\t\t\t|     MENU ADMIN - SALAS   |";
+            cout << "\n\t\t\t\t\t----------------------------";
+            cout << "\n\n\n\n\t\t\t . Escolha o que deseja fazer: \n\n";
+            cout << "\n\n\n\t\t\t1. Adicionar sala";
+            cout << "\n\n\t\t\t2. Remover sala";
+            cout << "\n\n\t\t\t3. Procurar sala";
+            cout << "\n\n\t\t\t4. Voltar no menu anterior\n\n\t\t\t";
+            cin >> aux_menu_salas;
 
-        } else if (aux_menu == "4"){
+            if(aux_menu_salas == "1"){
+                criar_sala();
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_salas == "2"){
+                aux_excluir = excluir_sala();
+                if(aux_excluir == 0){
+                    cout << "\n\n\n\n\t\t\tNenhuma sala com esse ID!";
+                    system("pause>nul");
+                }
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_salas == "3"){
+                aux_buscar = buscar_sala();
+                if(aux_buscar == 0){
+                    cout << "\n\n\n\n\t\t\tNenhuma sala com esse ID!";
+                    system("pause>nul");
+                }
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_salas == "4"){
+                return 7;
+            }
+        } else if (aux_menu == "4"){ /// Clientes
+            system("cls");
+            string aux_menu_clientes;
 
+            cout << "\n\t\t\t\t\t----------------------------";
+            cout << "\n\t\t\t\t\t|   MENU ADMIN - CLIENTES  |";
+            cout << "\n\t\t\t\t\t----------------------------";
+            cout << "\n\n\n\n\t\t\t . Escolha o que deseja fazer: \n\n";
+            cout << "\n\n\n\t\t\t1. Adicionar cliente";
+            cout << "\n\n\t\t\t2. Remover cliente";
+            cout << "\n\n\t\t\t3. Procurar cliente";
+            cout << "\n\n\t\t\t4. Voltar no menu anterior\n\n\t\t\t";
+            cin >> aux_menu_clientes;
+
+            if(aux_menu_clientes == "1"){
+                criar_cliente();
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_clientes == "2"){
+                aux_excluir = excluir_cliente();
+                if(aux_excluir == 0){
+                    cout << "\n\n\n\n\t\t\tNenhum cliente com esse ID!";
+                    system("pause>nul");
+                }
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_clientes == "3"){
+                aux_buscar = buscar_cliente();
+                if(aux_buscar == 0){
+                    cout << "\n\n\n\n\t\t\tNenhum cliente com esse ID!";
+                    system("pause>nul");
+                }
+                menu_admin(vet_filmes, aux);
+            } else if(aux_menu_clientes == "4"){
+                return 7;
+            }
         } else if (aux_menu == "5"){
-            return 1;
+            return 8;
         }
     } else if (aux == 4) {
-        cout << "\n\n\n\n\n\t\t\tSenha incorreta! ";
-        system("pause>nul");
-        return menu_admin(vet_filmes);
+        int confirma;
+
+        cout << "\n\n\n\t\t\t1. Tentar novamente";
+        cout << "\n\n\t\t\t2. Retornar ao menu anterior\n\n\t\t\t";
+        cin >> confirma;
+
+        if (confirma == 1){
+            aux = confirma_identidade();
+            menu_admin(vet_filmes, aux);
+        } else if (confirma == 2){
+            return 15;
+        }
     } else if (aux == 3){
-        cout << "\n\n\n\n\n\t\t\tEste usuário não consta no nosso sistema! ";
-        system("pause>nul");
-        return menu_admin(vet_filmes);
+        int confirma;
+
+        cout << "\n\n\n\t\t\t1. Tentar novamente";
+        cout << "\n\n\t\t\t2. Retornar ao menu anterior\n\n\t\t\t";
+        cin >> confirma;
+
+        if (confirma == 1){
+            aux = confirma_identidade();
+            menu_admin(vet_filmes, aux);
+        } else if (confirma == 2){
+            return 15;
+        }
     }
 }
 
-int menu_acesso(filmes vet_filmes[]){
+int menu_acesso(filmes vet_filmes[], int &aux){
     string select;
+    int verifica;
 
+    system("cls");
     cout << "\n\t\t\t\t\t----------------------------";
     cout << "\n\t\t\t\t\t| CINECON - MENU DE ACESSO |";
     cout << "\n\t\t\t\t\t----------------------------";
@@ -227,7 +572,11 @@ int menu_acesso(filmes vet_filmes[]){
     if (select == "1") { // Caso a tecla pressionada seja 1
         menu_cliente(vet_filmes);
     } else if (select == "2") {
-        menu_admin(vet_filmes);
+        aux = confirma_identidade();
+        verifica = menu_admin(vet_filmes, aux);
+        if (verifica == 8){
+            menu_acesso(vet_filmes, aux);
+        }
     }
 }
 
