e671985d1c3862d6be19c3161d327933de8b3034
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2024-05-27 15:45:14 -0300

    Delete SO/M2/Round-Robin com Prioridade directory

--- SO/M2/Round-Robin com Prioridade/CPU.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "task.h"
-
-// Executa esta tarefa pelo tempo especificado
-void run(Task *task, int slice) {
-    printf("Running task = [%s] [%d] [%d] for %d units\n",
-    task->name, task->priority, task->burst, slice);
-}
